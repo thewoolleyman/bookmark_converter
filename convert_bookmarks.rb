a30e1066dbb9490e4c94de5ce319be5840694d5a
Chad Woolley <thewoolleyman@gmail.com>
2009-10-12 10:37:55 -0700

    add instructions

--- convert_bookmarks.rb
+++ convert_bookmarks.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
+# Copy this file into a dir containing *.URL files with lines like "URL=..." and pipe the output to a html file
+# chmod a+x convert_bookmarks.rb
+# ./convert_bookmarks.rb > my_bookmarks.html
+
 files = Dir.entries(File.expand_path(File.dirname(__FILE__)))
 files.reject! {|f| f == '.' or f == '..'}
+files.reject! {|f| File.directory?(f)}
 files.sort!
 urls = []
 files.each do |file|
